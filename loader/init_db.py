2a1c28b289abde5d3a3b756352a4b4f8f8114700
Benjamin Egelund-Müller <b@egelund-muller.com>
2017-10-20 18:25:37 +0400

    Cleanup part 2

--- loader/init_db.py
+++ loader/init_db.py
@@ -2,11 +2,8 @@
 
 import database
 
-def event_handler(event, context):
-    print("Creating blocks table")
-    database.create_blocks_table()
+print("Creating blocks table")
+database.create_blocks_table()
 
-    print("Creating transactions table")
-    database.create_transactions_table()
-
-    return "Success!"
+print("Creating transactions table")
+database.create_transactions_table()
--- loader/processor.py
+++ loader/processor.py
@@ -20,31 +20,17 @@ def process_block(block_number):
         except psycopg2.IntegrityError:
             print("Transaction <{}> already loaded".format(tx_hash))
 
-# def event_handler(event, context):
-#     block_number = int(event["block"])
-#     print("Processing block <{}>".format(block_number))
-#     process_block(block_number)
-#     return "Success!"
-
-# """ Reads block number from argv and loads block """
-# def process():
-#     if len(sys.argv) != 2:
-#         print("Pass block number as argument")
-#         return
-#     block_number = int(sys.argv[1])
-#     process_block(block_number)
-#
-# process()
-
-"""  """
-def process_range():
+""" Processes blocks as per CLI args """
+def process():
     if len(sys.argv) != 3:
         print("Pass block number and number of blocks as arguments")
         return
+
     block_number = int(sys.argv[1])
     blocks = int(sys.argv[2])
+    
     for i in range(0, blocks):
         process_block(block_number + i)
         print("Processed block <{}>".format(block_number + i))
 
-process_range()
+process()
