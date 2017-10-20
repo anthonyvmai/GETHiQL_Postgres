3c692dad2bd2c000b50a3a741345138f392d0028
Anthony Mai <amai@appnexus.com>
2017-10-20 09:48:35 +0400

    make work for osx

--- initdb_osx.sh
+++ initdb_osx.sh
@@ -7,9 +7,9 @@ passfile=pgpass.conf
 create_tables=create_tables.sql
 
 echo "Creating db $db"
-sudo -u postgres bash -c "createdb $db"
+createdb $db
 echo "Creating tables from $create_tables"
-sudo -u postgres bash -c "psql -d $db -f $create_tables"
+psql -d $db -f $create_tables
 echo "Creating psql user $user with password $pass"
-sudo -u postgres bash -c "psql -c \"CREATE USER $user WITH PASSWORD '$pass';\""
+psql -c "CREATE USER $user WITH PASSWORD '$pass';"
 echo "done"
