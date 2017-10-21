73d145d0c7b6ff86f779b8b4829c90521d7a8bcb
Akua Nti <afriyie.nti@gmail.com>
2017-10-21 11:03:49 +0400

    Improvements to monitoring script.

--- loader/monitor_blockchain.py
+++ loader/monitor_blockchain.py
@@ -1,3 +1,4 @@
+import argparse
 import time
 
 import scraper
@@ -8,25 +9,39 @@ web3 = scraper.web3
 
 
 if __name__ == '__main__':
-    # continuously look for the latest block
-    seconds_per_block = 15
-    batch_size = 10
+    parser = argparse.ArgumentParser(description='Continuously look for the latest block')
+    parser.add_argument('--interval', type=int, default=15, help='Polling interval')
+    parser.add_argument('--batch-size', type=int, default=0,
+                        help='Batch size: 0 to process all available')
+    args = parser.parse_args()
+
+    polling_interval = args.interval
+    batch_size = args.batch_size
 
     while True:
-        latest = web3.eth.blockNumber
-        print('latest block: {}'.format(latest))
+        try:
+            latest = web3.eth.blockNumber
+            print('latest block: {:,}'.format(latest))
 
-        last_seen = database.max_block()
-        print('last seen: {}'.format(last_seen))
+            last_seen = database.max_block()
+            print('last seen: {:,}'.format(last_seen))
 
-        # print(database.last_block())
+            # print(database.last_block())
 
-        # if the latest block is greater than our latest block, fetch data
-        if latest > last_seen:
-            # print('new batch')
-            first = last_seen + 1
-            to_process = min(latest - first, batch_size)
-            processor.process(first, to_process)
-            last_seen += to_process
+            # if the latest block is greater than our latest block, fetch data
+            if latest > last_seen:
+                first = last_seen + 1
 
-        time.sleep(seconds_per_block)
+                if batch_size == 0:
+                    to_process = latest - first
+                else:
+                    to_process = min(latest - first, batch_size)
+
+                print('Processing {:,} blocks...'.format(to_process))
+                processor.process(first, to_process)
+                last_seen += to_process
+
+            time.sleep(polling_interval)
+        except KeyboardInterrupt:
+            print('interrupted, exiting...')
+            break
